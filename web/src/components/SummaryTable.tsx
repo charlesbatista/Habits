9af83e4afdc33063d2b16eacdac67029877847fa
Charles Batista <charlesbatista@hotmail.com>
2023-01-22 21:45:51 -0300

    Aula 4 finalizada

--- web/src/components/SummaryTable.tsx
+++ web/src/components/SummaryTable.tsx
@@ -1,3 +1,6 @@
+import dayjs from "dayjs";
+import { useEffect, useState } from "react";
+import { api } from "../lib/axios";
 import { generateYearDates } from "../utils/generate-year-dates";
 import { HabitDay } from "./HabitDay";
 
@@ -8,7 +11,22 @@ const summaryDates = generateYearDates();
 const minimumSummaryDatesSize = 18 * 7; // 18 semanas
 const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length;
 
+type Summary = {
+  id: string;
+  date: string;
+  amount: number;
+  completed: number;
+}[];
+
 export function SummaryTable() {
+  const [summary, setSummary] = useState<Summary>([]);
+
+  useEffect(() => {
+    api.get("summary").then((response) => {
+      setSummary(response.data);
+    });
+  }, []);
+
   return (
     <div className="w-full flex">
       <div className="grid grid-rows-7 grid-flow-row gap-3">
@@ -26,13 +44,18 @@ export function SummaryTable() {
 
       <div className="grid grid-rows-7 grid-flow-col gap-3">
         {summaryDates.map((d) => {
+          const dayInSummary = summary.find((day) => {
+            return dayjs(d).isSame(day.date, 'day');
+          });
+
           return (
-          <HabitDay 
-          key={d.toDateString()} 
-          amount={5} 
-          completed={Math.round(Math.random() * 5)}
-           />
-          )
+            <HabitDay
+              key={d.toDateString()}
+              date={d}
+              amount={dayInSummary?.amount}
+              completed={dayInSummary?.completed}
+            />
+          );
         })}
 
         {amountOfDaysToFill > 0 &&
